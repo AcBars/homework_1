52e6bf4163205b312e02dd3770678b9cac8d6206
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-08 21:05:48 +0300

    task_6.ver.001

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+int number = Convert.ToInt32(ReadLine());
+if (Math.Abs(number%2)>0) WriteLine("Число нечетное");
+else WriteLine("Число четное");

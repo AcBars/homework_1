b5fc5d24eba6ebb31ba1274dae8c7bfea3e717ee
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-08 21:25:53 +0300

    task_8.ver.001

---
+++ task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+int number = Convert.ToInt32(ReadLine());
+for (int count=2; count<=number; count=count+2)
+{
+    WriteLine(count);
+}
